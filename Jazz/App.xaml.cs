d1b77ff9040dd4a6e19de199b4716db17ae191bf
Влад Козлов <vkozlov15012005@gmail.com>
2025-03-03 20:06:35 +0300

    Добавьте файлы проекта.

---
+++ Jazz/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Jazz.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Jazz
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static JazzDbEntities _context;
+        public static JazzDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new JazzDbEntities();
+            }
+            return _context;
+        }
+    }
+}
